9b13b393e281d51593978de1ccc7c11727b57883
SOURAV MANDAL <sourav0200@gmail.com>
2022-11-11 22:06:22 +0600

    New codes are added

---
+++ Adder.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Adder extends Arithmetic{  
+    public int callAdd(int a, int b){
+        return add(a, b);
+    }
+}
---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Arithmetic{
+    public int add(int a, int b){
+        int sum = a + b; 
+        return sum;
+    }
+}
